bb825dc2867243e390f291438c4e083ec32de09e
mohamedj <mohamedj0910@gmail.com>
2026-01-17 23:38:54 +0530

    chore(lib): Library deps added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isLib = mode === 'lib'
+
+  return {
+    plugins: [
+      !isLib && react(),
+      isLib && cssInjectedByJsPlugin(),
+      isLib &&
+        dts({
+          tsconfigPath: './tsconfig.app.json',
+          insertTypesEntry: true,
+          rollupTypes: true,
+        }),
+    ].filter(Boolean),
+
+    build: isLib
+      ? {
+          lib: {
+            entry: path.resolve(__dirname, 'src/lib/index.ts'),
+            name: 'ReactColorPicker',
+            formats: ['es', 'cjs'],
+            fileName: (format) => `index.${format}.js`,
+          },
+
+          rollupOptions: {
+            // Prevent bundling React
+            external: ['react', 'react-dom'],
+            output: {
+              globals: {
+                react: 'React',
+                'react-dom': 'ReactDOM',
+              },
+            },
+          },
+          cssCodeSplit: true,
+        }
+      : {
+          outDir: 'dist',
+        },
+  }
 })
